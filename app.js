e20161cc3973653c63eca3ce28a9e1180c3e4192
Fernando Araujo <feraraujofilho@gmail.com>
2020-01-29 20:46:17 +0100

    auth without testing

---
+++ app.js
@@ -0,0 +1,59 @@
+require("dotenv").config();
+
+const bodyParser = require("body-parser");
+//const cookieParser = require("cookie-parser");
+const express = require("express");
+//const favicon = require("serve-favicon");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const flash = require("connect-flash");
+const passport = require("passport")
+
+mongoose
+  .connect("mongodb://localhost/travelproject", {
+    useNewUrlParser: true
+  })
+  .then(x => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch(err => {
+    console.log("Error occured", err);
+  });
+
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
+
+const app = express();
+
+//Middleware Setup
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(cookieParser())
+
+app.locals.title = "Travel Portal";
+
+app.use(
+  session({
+    secret: process.env.SECRETKEY,
+    resave: true,
+    saveUninitialized: true,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+  })
+);
+
+app.use(flash());
+require("./passport")(app);
+
+//routes
+const authRoutes = require("./routes/auth")
+app.use("/api/auth", authRoutes)
+
+module.exports = app
