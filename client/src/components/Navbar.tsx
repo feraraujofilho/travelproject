78c9c533bd7ca7a9c81f63118ef578f31ded4df0
Fernando Araujo <feraraujofilho@gmail.com>
2020-01-31 15:07:00 +0100

    Navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,17 +1,59 @@
-import React, { Component } from 'react'
-import Logo from "../img/Logo-Test.png"
+import React, { Component } from "react";
+import Logo from "../img/Logo-Test.png";
+import styled from "styled-components";
+import { fontFamily, fontSize, btnRed } from "./Styles";
+import { Link } from "react-router-dom"
+
+const Container = styled.div`
+  font-size: ${fontSize};
+
+  .navbar {
+    display: flex;
+    justify-content: space-between;
+    img {
+      height: 3.5em;
+      width: auto;
+    }
+    ul {
+      display: flex;
+      text-decoration: none;
+      list-style: none;
+      padding-right: 2em;
+      a {
+        padding: 5px;
+        text-decoration: none;
+        color: black;
+        &:hover {
+          color: ${btnRed};
+        }
+      }
+    }
+  }
+  @media only screen and (max-width: 768px) {
+    font-size: 18px;
+    .navbar {
+      flex-direction: column;
+      align-items: center;
+      ul {
+        margin: 0;
+      }
+    }
+  }
+`;
 
 export default class Navbar extends Component {
   render() {
     return (
-      <div className="navbar">
-        <img src={Logo} alt="logo"/>
-        <ul>
-          <li>Home</li>
-          <li>Search</li>
-          <li>Contact</li>
-        </ul>
-      </div>
-    )
+      <Container>
+        <div className="navbar">
+          <img src={Logo} alt="logo" />
+          <ul>
+            <Link to="/">Home</Link>
+            <Link to="/search">Search</Link>
+            <Link to="/contact">Contact</Link>
+          </ul>
+        </div>
+      </Container>
+    );
   }
 }
---
+++ client/src/components/Styles.jsx
@@ -0,0 +1,7 @@
+// colors
+export const btnRed = "rgb(255,64,72)"
+export const bgBlue = "rgb(53,120,181)"
+
+//fonts
+export const fontFamily = "'Poppins', 'Helvetica', 'Arial', 'sans-serif'";
+export const fontSize = "16px"; 
